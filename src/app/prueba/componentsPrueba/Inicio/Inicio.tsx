053b741e04775b980cf8398723b305c51549edd6
rken2010 <90514981+rken2010@users.noreply.github.com>
2023-11-29 19:09:57 -0300

    Add V2 for components view

---
+++ src/app/prueba/componentsPrueba/Inicio/Inicio.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Inicio({}: Props) {
+  return (
+    <div>Inicio</div>
+  )
+}
---
+++ src/app/prueba/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Inicio from '../prueba/componentsPrueba/Inicio/Inicio'
+
+type Props = {}
+
+const Prueba = (props: Props) => {
+  return (
+    <div>
+        <Inicio />
+    </div>
+  )
+}
+
+export default Prueba
