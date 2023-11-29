bd3a9c0b4e3286e19d9e2f37395cd45fcf532672
rken2010 <rkenshin.2010@gmail.com>
2023-11-29 20:11:21 -0300

    style: :bookmark: Add new version home in mobile and desktop

--- src/app/prueba/componentsPrueba/Inicio/Inicio.tsx
+++ src/app/prueba/componentsPrueba/Inicio/Inicio.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from "./inicio.module.css"
 
 type Props = {}
 
 export default function Inicio({}: Props) {
   return (
-    <div>Inicio</div>
+    <section className={styles.home__container}>
+      <h2 className={styles.home__title}>Tu hogar, <br />tu refugio</h2>
+      <h3 className={styles.home__subtitle}>Nosotros lo hacemos <span>REALIDAD</span></h3>
+    </section>
   )
 }
