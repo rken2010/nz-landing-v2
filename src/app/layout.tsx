54f227bb1880e715249cb01c2271748f815d7aa4
rken2010 <90514981+rken2010@users.noreply.github.com>
2023-12-12 11:46:05 -0300

    add script google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter  } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+
+
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-4693MBSRD8"></Script>
+<Script id='google-analytics'> {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4693MBSRD8');
+`}
+</Script>
+
       <body className={inter.className}>{children}</body>
+
+
+      
     </html>
   )
 }
